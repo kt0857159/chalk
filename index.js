c3eae04481f7a436d8dfdb289ee52006ba71de37
unknown <kt0857159@otc.edu>
2017-10-30 22:41:59 -0500

    Added short story

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const chalk = require('chalk');
+const r = chalk.red.underline;
+const g = chalk.grey.underline;
+const w = chalk.underline;
+const log = console.log;
+
+log(r('There ') + g('was ') + w('once ') + r('an ') + g('irresponsible ') + w('college ') +
+r('student ') + g('attempting ') + w('to ') + r('finish ') + g('an ') + w('assignment ') +
+r('for ') + g('his ') + w('favorite ') + r('class ') + g('in ') + w('the ') + r(' entire ') 
++ g('world! ') + w('Somewhere ') + r('in ') + g('the ') + w('middle ') + r('of ') + g('this ') 
++ w('assignment ') + r('he ') + g('decided ') + w('it ') + r('would ') + g('be ') + w('a ') +
+r('good ') + g('idea ') + w('to ') + r('order ') + g('insomnia ') + w('cookies! ') + r('If ') +
+g("you've ") + w('ever ') + r('had ') + g('them ') + w('then ') + r('you ') + g('know ') +
+w('that ') + r('it ') + g('was ') + w('in ') + r('fact ') + g('a ') + w('good ') + r('idea! ') +
+g('The ') + w('End!'));
+log(chalk.green.underline('But for real if you have not had them you definitely need to!!'))
